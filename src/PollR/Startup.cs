83a0e9ca5d77609b76400dd1ac60ab71072e4bc4
hgcummings <harry@xlr3.com>
2014-10-11 22:46:11 +0100

    Initial commit ('Hello World' MVC application)

---
+++ src/PollR/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Diagnostics;
+using Microsoft.AspNet.Routing;
+
+namespace PollR
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseErrorPage(ErrorPageOptions.ShowAll);
+
+            app.UseServices(services =>
+            {
+                services.AddMvc();
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    null,
+                    "{controller}/{action}",
+                    new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
