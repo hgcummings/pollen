1001cdce68bf7af458676fed401a92607f4e78d4
hgcummings <harry@xlr3.com>
2014-10-13 23:26:01 +0100

    Rudimentary voting

--- src/PollR/Model/Poll.cs
+++ src/PollR/Model/Poll.cs
@@ -7,22 +7,48 @@ namespace PollR.Model
 {
     public class Poll
     {
-        private readonly ConcurrentDictionary<string, int> votes;
+        private readonly ConcurrentDictionary<string, int> options;
+        private readonly ConcurrentDictionary<string, string> votes;
 
         public Poll()
         {
-            votes = new ConcurrentDictionary<string, int>();
+            options = new ConcurrentDictionary<string, int>();
+            votes = new ConcurrentDictionary<string, string>();
         }
 
         public void AddOption(string name)
         {
-            votes.GetOrAdd(name, 0);
+            options.GetOrAdd(name, 0);
         }
 
-        public IEnumerable<string> Options
+        public IEnumerable<KeyValuePair<string, int>> Options
         {
-            get { return votes.Keys; }
+            get { return options; }
         }
-        
+
+        public IEnumerable<KeyValuePair<string, int>> Vote(string option, string connectionId)
+        {
+            return CancelPreviousVote(connectionId).Concat(RecordNewVote(option, connectionId));
+        }
+
+        private IEnumerable<KeyValuePair<string, int>> CancelPreviousVote(string connectionId)
+        {
+            string prevVote;
+
+            if (votes.TryRemove(connectionId, out prevVote))
+            {
+                var newValue = options.AddOrUpdate(prevVote, x => 0, (x, prevValue) => prevValue - 1);
+                return new[] { new KeyValuePair<string, int>(prevVote, newValue) };
+            }
+
+            return Enumerable.Empty<KeyValuePair<string, int>>();
+        }
+
+        private IEnumerable<KeyValuePair<string, int>> RecordNewVote(string option, string connectionId)
+        {
+            var newValue = options.AddOrUpdate(option, x => 1, (x, prevValue) => prevValue + 1);
+            votes.AddOrUpdate(connectionId, x => option, (x, y) => option);
+            return new[] { new KeyValuePair<string, int>(option, newValue) };
+        }
     }
 }
--- src/PollR/PollHub/PollHub.cs
+++ src/PollR/PollHub/PollHub.cs
@@ -1,11 +1,35 @@
 ﻿using System;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using PollR.Model;
 
 namespace PollR.PollHub
 {
     [HubName("poll")]
     public class PollHub : Hub
     {
+        private readonly PollRepository pollRepository;
+
+        public PollHub(PollRepository pollRepository)
+        {
+            this.pollRepository = pollRepository;
+        }
+
+        public void Register()
+        {
+            foreach (var option in this.pollRepository.GetCurrentPoll().Options)
+            {
+                Clients.Caller.UpdateOption(option.Key, option.Value);
+            }
+        }
+
+        public void Vote(string option)
+        {
+            var updated = this.pollRepository.GetCurrentPoll().Vote(option, Context.ConnectionId);
+            foreach (var update in updated)
+            {
+                Clients.All.UpdateOption(update.Key, update.Value);
+            }
+        }
     }
 }
